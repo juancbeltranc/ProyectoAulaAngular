e72f34ed6c324805decefa7d398b90b0c03715fc
Juanito <juancbeltranc@gmail.com>
2021-11-27 13:34:53 -0500

    2do commit

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import{User} from 'src/app/models/user'
 import {UsuarioService} from 'src/app/services/usuario.service'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -18,9 +19,56 @@ export class UserDetailsComponent implements OnInit {
 	    direccion: "",
 	    ciudad: "" 
   };
-  constructor() { }
+
+  message = '';
+  constructor(
+    private userService: UsuarioService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
 
   ngOnInit(): void {
+    if (!this.viewMode) {
+      this.message = '';
+      this.getUser(this.route.snapshot.params["id"]);
+    }
+  }
+
+  getUser(id: string): void {
+    this.userService.get(id)
+      .subscribe({
+        next: (data) => {
+          this.currentTutorial = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+
+
+  updateUser(): void {
+    this.message = '';
+
+    this.userService.update(this.currentTutorial.cedula, this.currentTutorial)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.message = res.message ? res.message : '!El usuario fue modificado correctamente!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteUser(): void {
+    this.userService.delete(this.currentTutorial.cedula)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/user']);
+        },
+        error: (e) => console.error(e)
+      });
   }
 
 }
